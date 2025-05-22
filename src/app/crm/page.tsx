9f742174f7fb32701d288194737b36e857393bb6
truesoulcoder <chrisphillips@truesoulpartners.com>
2025-05-21 19:46:25 -0500

    feat: add CRM page with dynamic view component for employee lead management

--- src/app/crm/page.tsx
+++ src/app/crm/page.tsx
@@ -6,16 +6,9 @@ import React from 'react';
 // Import CrmView component
 import CrmView from '@/components/views/CrmView';
 
-// Dynamically import the GoogleMapsLoader with SSR disabled
-const GoogleMapsLoader = dynamic(
-  () => import('@/components/maps/GoogleMapsLoader'),
-  { ssr: false }
-);
 
 export default function CrmPage() {
   return (
-    <GoogleMapsLoader>
-      <CrmView />
-    </GoogleMapsLoader>
+    <CrmView />
   );
 }
