27bd81f5c6bc1ad21785ecea55dce05e84a72074
TrueSoulCoder <chrisphillips@truesoulpartners.com>
2025-05-25 04:15:37 -0500

    feat: implement Supabase client utilities for admin and server-side operations

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -3,11 +3,21 @@ import { cookies } from 'next/headers'
 
 import type { Database } from '@/utils/supabase/database'
 
-export const createSupabaseServerClient = () => {
-  const cookieStore = cookies()
-  
-  return createServerClient<Database>(
+export const createSupabaseServerClient = () =>
+  createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookies().get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookies().set({ name, value, ...options })
+        },
+        remove(name: string, options: CookieOptions) {
+          cookies().set({ name, value: '', ...options })
+        }
+      }
+    }
   )
-}
